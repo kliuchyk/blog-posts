d92844ed8681dccd84a6e5101ad3c529357a5426
kliuchyk <kliuchyk@gmail.com>
2020-01-04 16:15:08 +0200

    add PostsList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header';
+import PostsList from './components/posts-list/PostsList';
+
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div>
       <Header />
+      <PostsList />
       <div>Blog App</div>
     </div>
   );
---
+++ src/components/posts-list/PostsList.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PostsList.css';
+
+export const PostsList = () => {
+  return <div>Post List</div>;
+};
+
+export default PostsList;
