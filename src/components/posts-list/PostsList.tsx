663ef5b79365d160adc4c0875cf5defa70c87e73
kliuchyk <kliuchyk@gmail.com>
2020-01-05 13:56:28 +0200

    render post list with react redux hooks

--- src/components/posts-list/PostsList.tsx
+++ src/components/posts-list/PostsList.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import PostItem from '../post-item/PostItem';
+import {fetchPosts} from '../../actions';
 import './PostsList.css';
 
 export const PostsList = () => {
-  return <div>Post List</div>;
+  const posts = useSelector((state: any) => state.posts.items);
+  const isLoading = useSelector((state: any) => state.posts.isFetching);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const renderedList = isLoading
+    ? 'Loading from server...'
+    : posts.map((post: any) => <PostItem key={post.id} />);
+
+  return (
+    <div>
+      <h1>Post List</h1>
+      {renderedList}
+    </div>
+  );
 };
 
 export default PostsList;
