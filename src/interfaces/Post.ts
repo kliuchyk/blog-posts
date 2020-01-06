640c7d3e35edf3ee5428b2fcafc24855cad33c0f
kliuchyk <kliuchyk@gmail.com>
2020-01-06 10:18:29 +0200

    add post interface

---
+++ src/interfaces/Post.ts
@@ -0,0 +1,5 @@
+export default interface Post {
+  body: string;
+  id: number;
+  title: string;
+}
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import {REQUEST_POSTS, SELECT_POST, RECEIVE_POSTS, CREATE_POST} from '../actions';
+import Post from '../interfaces/Post';
 
 const initialState = {
   isFetching: false,
   items: [],
+  selectedPost: null,
 };
 
 function posts(state = initialState, action: any) {
@@ -12,12 +14,18 @@ function posts(state = initialState, action: any) {
       return {
         ...state,
         isFetching: true,
+        // selectedPost: null,
       };
     case RECEIVE_POSTS:
       return {
         ...state,
         isFetching: false,
         items: action.posts,
+      };
+    case SELECT_POST:
+      return {
+        ...state,
+        selectedPost: state.items.find((post: Post) => post.id === action.id),
       };
     default:
       return state;
