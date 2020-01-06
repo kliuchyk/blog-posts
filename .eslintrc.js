075f0c19325624a3a7474faa7ad91a584a809d26
kliuchyk <kliuchyk@gmail.com>
2020-01-06 10:33:52 +0200

    remove indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,10 +33,5 @@ module.exports = {
   },
   rules: {
     'react/jsx-filename-extension': [2, {extensions: ['.js', '.jsx', '.ts', '.tsx']}],
-    // 'import/no-extraneous-dependencies': [
-    //   2,
-    //   {devDependencies: ['**/test.tsx', '**/test.ts']},
-    // ],
-    '@typescript-eslint/indent': [2, 2],
   },
 };
